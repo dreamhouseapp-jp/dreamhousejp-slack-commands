2c2351c863714917bb422f458760c8c2ecadf125
ccoenraets <ccoenraets@gmail.com>
2016-05-05 11:21:16 -0400

    first commit

---
+++ modules/house.js
@@ -0,0 +1,32 @@
+let salesforce = require('./salesforce'),
+
+    HOUSE_TOKEN = process.env.HOUSE_TOKEN;
+
+exports.handle = (req, res) => {
+
+    if (req.body.token != HOUSE_TOKEN) {
+        res.send("Invalid token");
+        return;
+    }
+
+    let params  = req.body.text.split(" ");
+
+    if (params[0].toLowerCase() === "search") {
+        salesforce.findProperties({city: params[1], priceMin: params[2], priceMax: params[3], bedrooms: params[4]})
+            .then(properties => res.send({
+                text: "I found these properties:",
+                attachments: formatter.formatProperties(properties)
+            }))
+            .catch(error => res.send("Error while retrieving properties " + JSON.stringify(error)));
+    } else if (params[0].toLowerCase() === "changes") {
+        salesforce.findPriceChanges()
+            .then(priceChanges => res.send({
+                text: "Here are the recent price changes:",
+                attachments: formatter.formatProperties(priceChanges)
+            }))
+            .catch(error => res.send("Error while retrieving price changes " + JSON.stringify(error)));
+    } else {
+        res.send("I didn't understand what you asked");
+    }
+
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    house = require('./modules/house'),
+    app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/house', house.handle);
+
+app.listen(app.get('port'), () => console.log('Express server listening on port ' + app.get('port')));
