fc16108e62cecb1ce1013a3cb6733b5a598d0b09
ccoenraets <ccoenraets@gmail.com>
2016-05-05 11:48:28 -0400

    adding price changes

--- modules/formatter.js
+++ modules/formatter.js
@@ -4,7 +4,7 @@ let numeral = require("numeral");
 
 let color = "#009cdb";
 
-let formatProperties = properties => {
+exports.formatProperties = properties => {
 
     if (properties && properties.length>0) {
         let attachments = [];
@@ -23,4 +23,23 @@ let formatProperties = properties => {
 
 };
 
-exports.formatProperties = formatProperties;
+exports.formatPriceChanges = priceChanges => {
+
+    if (priceChanges && priceChanges.length>0) {
+        let attachments = [];
+        priceChanges.forEach(priceChange => {
+            let property = priceChange.get("Parent");
+            let fields = [];
+            fields.push({title: "Address", value: `${property.get("Address__c")}, ${property.get("City__c")} ${property.get("State__c")}`, short:true});
+            fields.push({title: "Link", value: "https://login.salesforce.com/" + property.getId(), short:true});
+            fields.push({title: "Bedrooms", value: property.get("Beds__c"), short:true});
+            fields.push({title: "Old Price", value: `${numeral(priceChange.get("OldValue")).format('$0,0')}`, short:true});
+            fields.push({title: "New Price", value: `${numeral(priceChange.get("NewValue")).format('$0,0')}`, short:true});
+            attachments.push({color: color, fields: fields});
+        });
+        return attachments;
+    } else {
+        return [{text: "No records"}];
+    }
+
+};
