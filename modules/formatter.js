1413a2e6253d08c3c475b7008606730a2b27fbba
ccoenraets <ccoenraets@gmail.com>
2016-05-05 12:07:35 -0400

    fix

--- modules/formatter.js
+++ modules/formatter.js
@@ -24,15 +24,14 @@ exports.formatProperties = properties => {
 };
 
 exports.formatPriceChanges = priceChanges => {
-    console.log(priceChanges);
     if (priceChanges && priceChanges.length>0) {
         let attachments = [];
         priceChanges.forEach(priceChange => {
             let property = priceChange.get("Parent");
+            console.log(property);
             let fields = [];
             fields.push({title: "Address", value: `${property.get("Address__c")}, ${property.get("City__c")} ${property.get("State__c")}`, short:true});
             fields.push({title: "Link", value: "https://login.salesforce.com/" + property.getId(), short:true});
-            fields.push({title: "Bedrooms", value: property.get("Beds__c"), short:true});
             fields.push({title: "Old Price", value: `${numeral(priceChange.get("OldValue")).format('$0,0')}`, short:true});
             fields.push({title: "New Price", value: `${numeral(priceChange.get("NewValue")).format('$0,0')}`, short:true});
             attachments.push({color: color, fields: fields});
